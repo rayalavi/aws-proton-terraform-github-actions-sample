463edf919a91a411ad1b0d02bb92a598b9cff4c2
sbx_user1051 <sbx_user1051@169.254.70.97>
2022-04-27 21:08:35 +0000

    Update requested for Proton - service/worker-ecs-fargate-sqs-svc/pipeline

---
+++ worker-ecs-fargate-sqs-svc/pipeline/config.tf
@@ -0,0 +1,39 @@
+/*
+This file is managed by AWS Proton. Any changes made directly to this file will be overwritten the next time AWS Proton performs an update.
+
+To manage this resource, see AWS Proton Resource: arn:aws:proton:ap-northeast-1:443437525071:service/worker-ecs-fargate-sqs-svc/pipeline
+
+If the resource is no longer accessible within AWS Proton, it may have been deleted and may require manual cleanup.
+*/
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+
+  backend "s3" {
+    region = "ap-northeast-1"
+    bucket = "terraform-samples-443437525071-worker-ecs-fargate-sqs-svc"
+    key    = "pipeline.tfstate"
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = var.aws_region
+  alias  = "default"
+
+  default_tags {
+    tags = {
+      "proton:pipeline" = var.service.name
+    }
+  }
+}
+
+variable "aws_region" {
+  type    = string
+  default = "us-east-1"
+} 
