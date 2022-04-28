7038bbeb32fd22c0f69a3916faff9ef9865191a9
sbx_user1051 <sbx_user1051@169.254.213.153>
2022-04-28 18:36:28 +0000

    Update requested for Proton - service/loadbalanced-ecs-ec2-svc/service-instance/loadbalanced-ecs-ec2-svc

--- ecs-ec2-env/loadbalanced-ecs-ec2-svc-loadbalanced-ecs-ec2-svc/main.tf
+++ ecs-ec2-env/loadbalanced-ecs-ec2-svc-loadbalanced-ecs-ec2-svc/main.tf
@@ -55,10 +55,12 @@ resource "aws_lb_target_group" "service_lb_public_listener_target_group" {
   port     = var.service_instance.inputs.port
   protocol = var.service_instance.inputs.loadbalancer_type == "application" ? "HTTP" : "TCP"
 
+  var.service_instance.inputs.loadbalancer_type == "application" ?
   stickiness {
-    enabled = var.service_instance.inputs.loadbalancer_type == "application" ? false : null
+    enabled = false
     type    = "lb_cookie"
-  }
+  } : null
+
   target_type = "instance"
   vpc_id      = var.environment.outputs.VpcId
 }
@@ -189,7 +191,6 @@ resource "aws_ecs_service" "service" {
 }
 
 resource "aws_security_group" "service_security_group" {
-  name        = "service_security_group"
   description = "Automatically created Security Group for the Service"
   vpc_id      = var.environment.outputs.VpcId
 
