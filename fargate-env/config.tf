d652edaa7354c9dded18d29d137cf086f225cfd4
sbx_user1051 <sbx_user1051@169.254.57.57>
2022-05-02 20:35:58 +0000

    Update requested for Proton - environment/fargate-env

--- fargate-env/config.tf
+++ fargate-env/config.tf
@@ -22,7 +22,7 @@ provider "aws" {
   region = var.aws_region
   default_tags {
     tags = {
-      environment = var.environment.name
+      proton:environment = var.environment.name
     }
   }
 }
