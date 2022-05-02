8e2293f07926ab8af1f057a5744f0358c605c456
sbx_user1051 <sbx_user1051@169.254.203.185>
2022-05-02 21:01:25 +0000

    Update requested for Proton - environment/fargate-env

--- fargate-env/main.tf
+++ fargate-env/main.tf
@@ -1,5 +1,9 @@
 /*
-This file is no longer managed by AWS Proton. The associated resource has been deleted in Proton.
+This file is managed by AWS Proton. Any changes made directly to this file will be overwritten the next time AWS Proton performs an update.
+
+To manage this resource, see AWS Proton Resource: arn:aws:proton:ap-northeast-1:443437525071:environment/fargate-env
+
+If the resource is no longer accessible within AWS Proton, it may have been deleted and may require manual cleanup.
 */
 
 module "vpc" {
@@ -34,9 +38,13 @@ resource "aws_sns_topic" "ping_topic" {
 
 resource "aws_ecs_cluster" "fargate_cluster" {
   name = "${var.environment.name}-Cluster"
-  capacity_providers = [
-    "FARGATE"
-  ]
+}
+
+resource "aws_ecs_cluster_capacity_providers" "fargate_cluster_capacity_providers" {
+  cluster_name = aws_ecs_cluster.fargate_cluster.name
+
+  capacity_providers = ["FARGATE"]
+
   default_capacity_provider_strategy {
     capacity_provider = "FARGATE"
   }
